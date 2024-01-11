a020e206c67879c9d991f8eaa189b0252966ae8e
“Nhud4” <nu12ulhud4@gmail.com>
2024-01-11 11:50:16 +0700

    fix(investments):add content

--- app/investments/page.tsx
+++ app/investments/page.tsx
@@ -1,11 +1,29 @@
 'use client'
 
 import React from 'react'
+import AmountIcon from '@/public/assets/icons/money-bag.svg'
+import InvestmentIcon from '@/public/assets/icons/pie-chart-1.svg'
+import ReturnIcon from '@/public/assets/icons/repeat.svg'
+import GeneralCard from '@/components/cards/generalCard'
 
 export default function Page() {
   return (
     <div>
-      <h1>Investments</h1>
+      <div className='grid grid-cols-3 gap-4'>
+        <GeneralCard
+          src={AmountIcon}
+          title='Total Invested Amount'
+          value='$150,000'
+          color='#DCFAF8'
+        />
+        <GeneralCard
+          src={InvestmentIcon}
+          title='Number of Investments'
+          value='1,250'
+          color='#FFE0EB'
+        />
+        <GeneralCard src={ReturnIcon} title='Rate of Return' value='+5.80%' color='#E7EDFF' />
+      </div>
     </div>
   )
 }
