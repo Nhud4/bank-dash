b52a56de0811bfd1ffc2f4f3de42278f993cb114
“Nhud4” <nu12ulhud4@gmail.com>
2024-01-11 10:42:23 +0700

    fix(transaction):add content

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,11 +1,21 @@
 'use client'
 
 import React from 'react'
+import CreditCard from '@/components/cards/creditCard'
+import Link from 'next/link'
 
 export default function Page() {
   return (
     <div>
-      <h1>Transaction</h1>
+      <div className='grid grid-cols-3 gap-4'>
+        <h1 className='text-[22px] font-semibold text-[#343C6A]'>My Cards</h1>
+        <Link href='#' className='text-[17px] font-semibold text-[#343C6A]'>
+          + Add Card
+        </Link>
+        <h1 className='text-[22px] font-semibold text-[#343C6A]'>My Expense</h1>
+        <CreditCard type='blue' />
+        <CreditCard type='white' />
+      </div>
     </div>
   )
 }
