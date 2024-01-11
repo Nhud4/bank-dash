5638e40b68837bb406e4cd35bc75d03c28dda1fb
“Nhud4” <nu12ulhud4@gmail.com>
2024-01-11 11:55:33 +0700

    fix(credit-card):add content

--- app/credit-cards/page.tsx
+++ app/credit-cards/page.tsx
@@ -1,11 +1,17 @@
 'use client'
 
 import React from 'react'
+import CreditCard from '@/components/cards/creditCard'
 
 export default function Page() {
   return (
     <div>
-      <h1>Credit Card</h1>
+      <div className='grid grid-cols-3 gap-4'>
+        <h1 className='col-span-3 text-[22px] font-semibold text-[#333B69]'>My Cards</h1>
+        <CreditCard type='blue' />
+        <CreditCard type='blue' />
+        <CreditCard type='white' />
+      </div>
     </div>
   )
 }
